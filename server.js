f467659a254706b05110631f18460ae4b0a0ab88
Sagar Jadhav <sagarmj1502@gmail.com>
2024-04-03 00:57:21 -0700

    without seprate route created public route for all html pages

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  //   res.send("<h1>Hello From Express</h1>");
-  res.sendFile(path.resolve(__dirname) + "/index.html");
-});
-app.get("/about", (req, res) => {
-  //   res.send("<h1>Hello From Express</h1>");
-  res.sendFile(path.resolve(__dirname) + "/about.html");
-});
+// app.get("/", (req, res) => {
+//   //   res.send("<h1>Hello From Express</h1>");
+//   res.sendFile(path.resolve(__dirname) + "/index.html");
+// });
+// app.get("/about", (req, res) => {
+//   //   res.send("<h1>Hello From Express</h1>");
+//   res.sendFile(path.resolve(__dirname) + "/about.html");
+// });
 
 app.listen(PORT, () => console.log(`Listening server on Port:${PORT}`));
