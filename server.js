e3c28ea3bad426d664d91271f271e99a3c29bd0e
Sagar Jadhav <sagarmj1502@gmail.com>
2024-04-03 02:06:18 -0700

    express basic routes created

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("public"));
 
-// app.get("/", (req, res) => {
-//   //   res.send("<h1>Hello From Express</h1>");
-//   res.sendFile(path.resolve(__dirname) + "/index.html");
-// });
-// app.get("/about", (req, res) => {
-//   //   res.send("<h1>Hello From Express</h1>");
-//   res.sendFile(path.resolve(__dirname) + "/about.html");
-// });
+app.get("/", (req, res) => {
+  //   res.send("<h1>Hello From Express</h1>");
+  res.sendFile(path.resolve(__dirname) + "/index.html");
+});
+app.get("/about", (req, res) => {
+  //   res.send("<h1>Hello From Express</h1>");
+  res.sendFile(path.resolve(__dirname) + "/about.html");
+});
+app.get("/download", (req, res) => {
+  //   res.send("<h1>Hello From Express</h1>");
+  res.download(path.resolve(__dirname) + "/about.html");
+});
 
 app.listen(PORT, () => console.log(`Listening server on Port:${PORT}`));
